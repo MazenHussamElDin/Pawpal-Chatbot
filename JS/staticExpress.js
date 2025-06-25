bbea9e8c271822f9e70efa0034c59a9d1fc656b0
MazenHussamElDin <126594723+MazenHussamElDin@users.noreply.github.com>
2025-06-25 02:02:07 +0200

    Update staticExpress.js

--- JS/staticExpress.js
+++ JS/staticExpress.js
@@ -29,7 +29,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '..')));
 
 app.post('/register', async (req, res) => {
   const { username, email, password, ownerType, dogSize, dogBreed } = req.body;
